5777bfdde042f30db92dba7a68bee4ab9b508fd8
Harrison Montoya <harrimv206@gmail.com>
2022-01-13 08:49:07 -0500

    Motores de pantillas

---
+++ MotoresDePlantillas/ejs/index.js
@@ -0,0 +1,40 @@
+const path= require('path');
+const express=require('express');
+
+
+const Datos = require('./data/data');
+const bodyParser = require('body-parser');
+
+const data = new Datos();
+
+const app=express();
+
+const PORT=8080;
+
+
+app.use(express.static('public'));
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', "./views");
+
+app.get("/productos",(req,res)=>{
+   let productos= data.listar()
+   res.render("index",{
+     productos
+   })
+})
+
+app.post("/productos",(req,res)=>{
+   const prod= req.body
+   data.guardar(prod)
+   res.redirect("/")
+})
+
+
+app.listen(PORT,()=>{
+   console.log(`Servidor escuchando por el puerto ${PORT}`)
+})
